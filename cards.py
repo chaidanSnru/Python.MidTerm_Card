3fbb95d55c3d03f0c442f334e3e566b2af570398
chaidanGmail <chaidan.ming@gmail.com>
2020-09-30 09:04:38 +0700

    ป็อกเด้งแบบแจกที่เดียว 2 ใบ

--- cards.py
+++ cards.py
@@ -17,6 +17,9 @@ players = ['Mr.M', 'Mr.N', 'Miss.O', 'Miss.P']
 
 for player in  players:
     playerDeck = []
+    totalPlayerPoint = 0
+    playerSymbol = []
+    playerNumber = []
     cardRandom = random.choice(cardDeck)
     playerDeck.append(cardRandom)
     cardDeck.remove(cardRandom)
@@ -24,3 +27,28 @@ for player in  players:
     playerDeck.append(cardRandom)
     cardDeck.remove(cardRandom)
     print(f'{player} ได้ไพ่เป็น {playerDeck}')
+
+    for playerCard in playerDeck:
+        playerSymbol.append(playerCard['symbol'])
+        playerNumber.append(playerCard['number'])
+        if playerCard['number'] not in ['10','J','Q','K','A']:
+            playerPoint = int(playerCard['number'])
+            totalPlayerPoint += playerPoint
+        else:
+            playerPoint = 0
+            totalPlayerPoint += playerPoint
+        if totalPlayerPoint >= 10:
+            totalPlayerPoint -= 10
+        if totalPlayerPoint > 7:
+            pokMessage = "ป็อก"
+        else:
+            pokMessage = ""
+        if len(playerSymbol) == 2:
+            if playerSymbol[0] == playerSymbol[1] or playerNumber[0] == playerNumber[1]:
+                dengMessage = "2 เด้ง"
+            else:
+                dengMessage = ""     
+        else:
+            pass
+            
+    print(f'เป็น {pokMessage} {totalPlayerPoint} แต้ม {dengMessage}')
